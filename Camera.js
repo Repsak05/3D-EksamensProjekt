9f900e8c2a5ef177b56c0ac81e703b81ee0e6fad
KasperL22R <119696654+Repsak05@users.noreply.github.com>
2024-05-19 21:19:34 +0200

    Camera class beginning

---
+++ Camera.js
@@ -0,0 +1,39 @@
+class Camera{
+    constructor(xPos, yPos, zPos)
+    {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.zPos = zPos; //FOV
+
+        this.moveSpeed = 5;
+    }
+
+    movement()
+    {
+        function keyPressed()
+        {
+            if(keyCode == 87) //w
+            {
+                this.zPos -= moveSpeed;
+            } else if(keyCode == 83){ //s
+                this.zPos += moveSpeed;
+            }
+
+            if(keyCode == 65) //a
+            {
+                this.xPos += moveSpeed;
+            } else if (keyCode == 68){ //d
+                this.xPos -= moveSpeed;
+            }
+
+            if(keyCode == 16) //Shift
+            {
+                this.yPos -= moveSpeed;
+            } else if(keyCode == 32) { //Space
+                this.yPos += moveSpeed;
+            }
+        }
+
+        keyPressed();
+    }
+}
